aaa81647c83d4fe053b09f4842a5141c8b8e4c14
Mojtaba <mojtaba.masoudi@tenoke.co.uk>
2020-04-03 19:12:44 +0100

    incomplete code of branch labeling

--- main.py
+++ main.py
@@ -35,6 +35,8 @@ from coreutils import get_imagepath, get_dirname, get_filename, get_fileroot
 # cwd = os.getcwd()
 # os.chdir(niftidir)
 
+
+
 niftidir = r'D:\Mojtaba\Dataset_test\9'
 os.chdir(niftidir)
 niftiname = 'L_coronary.nii.gz'
@@ -42,114 +44,118 @@ niftiroot = 'L_coronary'
 
 initial_surface = 'L_coronary_intersect.nii.gz'
 
-# automatically centerline calculation
-myargs = 'vmtkmarchingcubes -ifile ' + niftiname + ' -l 1.0 -ofile vessel_aorta_surface.vtp ' \
-          '--pipe vmtksurfaceviewer -array GroupIds'
-mypype = pypes.PypeRun(myargs)
-
-
-myargs = 'vmtksurfacesmoothing -iterations 3000 -ifile vessel_aorta_surface.vtp -ofile vessel_aorta_surface_smooth.vtp'
-mypype = pypes.PypeRun(myargs)
-
-
-myargs = 'vmtkmarchingcubes -ifile ' + initial_surface + ' -l 1.0 -ofile aorta_L_intersect_surface.vtp'
-mypype = pypes.PypeRun(myargs)
-
-
-# myargs = 'vmtksurfaceclipper -ifile initial_surface.vtp -ofile OutputFile1.vtp --pipe vmtksurfaceclipper' \
-#          ' -transform @.otransform -ifile vessel_aorta_surface.vtp -ofile OutputFile2.vtp'
+# # automatically centerline calculation
+# myargs = 'vmtkmarchingcubes -ifile ' + niftiname + ' -l 1.0 -ofile vessel_aorta_surface.vtp ' \
+#           '--pipe vmtksurfaceviewer -array GroupIds'
 # mypype = pypes.PypeRun(myargs)
-
-myargs = 'vmtksurfacecliploop -ifile vessel_aorta_surface_smooth.vtp' \
-         ' -i2file aorta_L_intersect_surface.vtp -ofile vessel_aorta_surface_clip.vtp'
-mypype = pypes.PypeRun(myargs)
-
-
-myargs = 'vmtksurfaceviewer -ifile vessel_aorta_surface_clip.vtp '
-mypype = pypes.PypeRun(myargs)
-
-myargs = 'vmtksurfaceviewer -ifile vessel_aorta_surface_clip.vtp '
-mypype = pypes.PypeRun(myargs)
-
-
-myargs = 'vmtknetworkextraction -ifile vessel_aorta_surface_clip.vtp -ofile vessel_aorta_centerline.vtp '
-          # '-ographfile output_graph.vtp '
-mypype = pypes.PypeRun(myargs)
-
-myargs = 'vmtklineresampling -ifile vessel_aorta_centerline.vtp -ofile vessel_aorta_centerline1.vtp -length 0.1'
-mypype = pypes.PypeRun(myargs)
-
-
-# myargs = 'vmtksurfaceviewer -ifile output_graph.vtp '
+#
+#
+# myargs = 'vmtksurfacesmoothing -iterations 2500 -ifile vessel_aorta_surface.vtp -ofile vessel_aorta_surface_smooth.vtp'
 # mypype = pypes.PypeRun(myargs)
-
-myargs = 'vmtksurfaceviewer -ifile vessel_aorta_centerline1.vtp '
-mypype = pypes.PypeRun(myargs)
-
-myargs = 'vmtksurfacereader -ifile vessel_aorta_surface_clip.vtp --pipe ' + \
-         'vmtkrenderer --pipe ' + \
-         'vmtksurfaceviewer -opacity 0.25' + ' --pipe ' + \
-         'vmtksurfaceviewer -ifile vessel_aorta_centerline1.vtp -array MaximumInscribedSphereRadius'
-
-mypype = pypes.PypeRun(myargs)
-
-
-# ----------------------------------------------------------------------------------------------------------------------
-# EXPORT CENTRELINE COORDINATES
-# ----------------------------------------------------------------------------------------------------------------------
-centreline_file = 'vessel_aorta_centerline1.vtp'
-
-
-centerlineReader = vmtkscripts.vmtkSurfaceReader()
-centerlineReader.InputFileName = centreline_file
-centerlineReader.Execute()
-
-clNumpyAdaptor = vmtkscripts.vmtkCenterlinesToNumpy()
-clNumpyAdaptor.Centerlines = centerlineReader.Surface
-clNumpyAdaptor.Execute()
-
-numpyCenterlines = clNumpyAdaptor.ArrayDict
-points = numpyCenterlines['Points']
-
-labels = numpyCenterlines['CellData']
-cell_points = labels['CellPointIds']
-topology = labels['Topology']
-
-for i in range(np.shape(cell_points)[0]):
-    number = np.shape(cell_points[i])[0]
-    index = np.ones(number) * (i+1)
-    if i == 0:
-        topology_labels = index
-    else:
-        topology_labels = np.append(topology_labels, index, axis=0)
-
-
-myargs = 'vmtkimagereader -ifile  ' + niftiname
-mypype = pypes.PypeRun(myargs)
-RasToIjkMatrixCoefficients = mypype.GetScriptObject('vmtkimagereader', '0').RasToIjkMatrixCoefficients
-XyzToRasMatrixCoefficients = mypype.GetScriptObject('vmtkimagereader', '0').XyzToRasMatrixCoefficients
-ras2ijk = np.asarray(RasToIjkMatrixCoefficients)
-ras2ijk = np.reshape(ras2ijk, (4, 4))
-xyz2ras = np.asarray(XyzToRasMatrixCoefficients)
-xyz2ras = np.reshape(xyz2ras, (4, 4))
-
-for x in range(points.shape[0]):
-    xyz = points[x]
-    xyz = np.append(xyz, 1)
-    ras = np.dot(xyz2ras, xyz)
-    ijk = np.dot(ras2ijk, ras)
-    ijk[3] = topology_labels[x]
-    if x == 0:
-        points_ijk = ijk
-    else:
-        points_ijk = np.append(points_ijk, ijk, axis=0)
-
-points_ijk = np.reshape(points_ijk, [x+1, 4])
-points_ijk = np.round(points_ijk[:, 0:4])
-print(points_ijk)
+#
+#
+# myargs = 'vmtkmarchingcubes -ifile ' + initial_surface + ' -l 1.0 -ofile aorta_L_intersect_surface.vtp'
+# mypype = pypes.PypeRun(myargs)
+#
+#
+# # myargs = 'vmtksurfaceclipper -ifile initial_surface.vtp -ofile OutputFile1.vtp --pipe vmtksurfaceclipper' \
+# #          ' -transform @.otransform -ifile vessel_aorta_surface.vtp -ofile OutputFile2.vtp'
+# # mypype = pypes.PypeRun(myargs)
+#
+# myargs = 'vmtksurfacecliploop -ifile vessel_aorta_surface_smooth.vtp' \
+#          ' -i2file aorta_L_intersect_surface.vtp -ofile vessel_aorta_surface_clip.vtp'
+# mypype = pypes.PypeRun(myargs)
+#
+#
+# myargs = 'vmtksurfaceviewer -ifile vessel_aorta_surface_clip.vtp '
+# mypype = pypes.PypeRun(myargs)
+#
+# myargs = 'vmtksurfaceviewer -ifile vessel_aorta_surface_clip.vtp '
+# mypype = pypes.PypeRun(myargs)
+#
+#
+# myargs = 'vmtknetworkextraction -ifile vessel_aorta_surface_clip.vtp -ofile vessel_aorta_centerline.vtp '
+#           # '-ographfile output_graph.vtp '
+# mypype = pypes.PypeRun(myargs)
+#
+# myargs = 'vmtklineresampling -ifile vessel_aorta_centerline.vtp -ofile vessel_aorta_centerline1.vtp -length 0.1'
+# mypype = pypes.PypeRun(myargs)
+#
+#
+# # myargs = 'vmtksurfaceviewer -ifile output_graph.vtp '
+# # mypype = pypes.PypeRun(myargs)
+#
+# myargs = 'vmtksurfaceviewer -ifile vessel_aorta_centerline1.vtp '
+# mypype = pypes.PypeRun(myargs)
+#
+# myargs = 'vmtksurfacereader -ifile vessel_aorta_surface_clip.vtp --pipe ' + \
+#          'vmtkrenderer --pipe ' + \
+#          'vmtksurfaceviewer -opacity 0.25' + ' --pipe ' + \
+#          'vmtksurfaceviewer -ifile vessel_aorta_centerline1.vtp -array MaximumInscribedSphereRadius'
+#
+# mypype = pypes.PypeRun(myargs)
+#
+#
+# # ----------------------------------------------------------------------------------------------------------------------
+# # EXPORT CENTRELINE COORDINATES
+# # ----------------------------------------------------------------------------------------------------------------------
+# centreline_file = 'vessel_aorta_centerline1.vtp'
+#
+#
+# centerlineReader = vmtkscripts.vmtkSurfaceReader()
+# centerlineReader.InputFileName = centreline_file
+# centerlineReader.Execute()
+#
+# clNumpyAdaptor = vmtkscripts.vmtkCenterlinesToNumpy()
+# clNumpyAdaptor.Centerlines = centerlineReader.Surface
+# clNumpyAdaptor.Execute()
+#
+# numpyCenterlines = clNumpyAdaptor.ArrayDict
+# points = numpyCenterlines['Points']
+#
+# labels = numpyCenterlines['CellData']
+# cell_points = labels['CellPointIds']
+# topology = labels['Topology']
+#
+# for i in range(np.shape(cell_points)[0]):
+#     number = np.shape(cell_points[i])[0]
+#     index = np.ones(number) * (i+1)
+#     if i == 0:
+#         topology_labels = index
+#     else:
+#         topology_labels = np.append(topology_labels, index, axis=0)
+#
+#
+# myargs = 'vmtkimagereader -ifile  ' + niftiname
+# mypype = pypes.PypeRun(myargs)
+# RasToIjkMatrixCoefficients = mypype.GetScriptObject('vmtkimagereader', '0').RasToIjkMatrixCoefficients
+# XyzToRasMatrixCoefficients = mypype.GetScriptObject('vmtkimagereader', '0').XyzToRasMatrixCoefficients
+# ras2ijk = np.asarray(RasToIjkMatrixCoefficients)
+# ras2ijk = np.reshape(ras2ijk, (4, 4))
+# xyz2ras = np.asarray(XyzToRasMatrixCoefficients)
+# xyz2ras = np.reshape(xyz2ras, (4, 4))
+#
+# for x in range(points.shape[0]):
+#     xyz = points[x]
+#     xyz = np.append(xyz, 1)
+#     ras = np.dot(xyz2ras, xyz)
+#     ijk = np.dot(ras2ijk, ras)
+#     ijk[3] = topology_labels[x]
+#     if x == 0:
+#         points_ijk = ijk
+#     else:
+#         points_ijk = np.append(points_ijk, ijk, axis=0)
+#
+# points_ijk = np.reshape(points_ijk, [x+1, 4])
+# points_ijk = np.round(points_ijk[:, 0:4])
+# print(points_ijk)
 
 csv_file = 'centreline_' + niftiroot + '.csv'
-np.savetxt(csv_file, points_ijk, delimiter=",")
+# np.savetxt(csv_file, points_ijk, delimiter=",")
+
+# branch labeling
+#  1. removing repetitive points for each label
+
 
 
 # centerline
@@ -164,6 +170,9 @@ print(df)
 centerline = np.zeros(np.shape(data1))
 df_numpy = np.round(df.to_numpy().astype(np.int32))
 
+# TODO
+# df_numpy = remove_repetitive(df_numpy)
+
 for i in range(df.values.shape[0]):
     x = df_numpy[i, 0]
     y = df_numpy[i, 1]
@@ -171,9 +180,17 @@ for i in range(df.values.shape[0]):
     label = df_numpy[i, 3]
     centerline[x, y, z] = label
 
+final = centerline
+
+centerline_name = 'centreline_' + niftiroot + '.nii.gz'
+mask = nib.Nifti1Image(final, img1.affine, img1.header)
+nib.save(mask, os.path.join(niftidir, centerline_name))
+
 
 # endpoint candidates
 index_endp_candidate = []
+label = np.max(df_numpy[:, 3])
+
 for i in range(label):
     index = np.argwhere(df_numpy[:, 3] == i+1)
     index_endp_candidate = np.append(index_endp_candidate, index[0], axis=0)
@@ -182,7 +199,33 @@ for i in range(label):
 labels = np.arange(label)
 labels = labels + 1
 
+
+# co-occurrence matrix between branches
+
+co_occurrence = np.zeros([label+1, label+1])
+patch_size = 2
+for i in range(label):
+
+    index = index_endp_candidate[(i*2)+1].astype(np.int32)
+    x = df_numpy[index, 0]
+    y = df_numpy[index, 1]
+    z = df_numpy[index, 2]
+    label = df_numpy[index, 3]
+
+    patch = centerline[x-patch_size:x+(patch_size+1),
+            y-patch_size:y+(patch_size+1),
+            z-1:z+2]
+    other_label = np.delete(labels, label-1)
+
+    neighbour_label = np.isin(other_label, patch)
+    a = np.where(neighbour_label == True)
+    b = other_label[a]
+    co_occurrence[label, b] = 1
+
+# endpoint estimation
 endpoints = np.zeros(np.shape(data1))
+patch_size = 1
+start_end = []
 
 for i in range(np.shape(index_endp_candidate)[0]):
 
@@ -193,7 +236,12 @@ for i in range(np.shape(index_endp_candidate)[0]):
     z = df_numpy[index, 2]
     label = df_numpy[index, 3]
 
-    patch = centerline[x-1:x+2, y-1:y+2, z-1:z+2]
+    a = [x, y, z, label]
+    start_end = np.append(start_end, a, axis=0)
+
+    patch = centerline[x-patch_size:x+(patch_size+1),
+            y-patch_size:y+(patch_size+1),
+            z-patch_size:z+(patch_size+1)]
     other_label = np.delete(labels, label-1)
 
     endpoint_or_not = np.isin(patch, other_label)
@@ -202,6 +250,20 @@ for i in range(np.shape(index_endp_candidate)[0]):
     else:
         endpoints[x, y, z] = 1
 
+
+# distance to aorta
+# TODO
+start_end = np.reshape(start_end, [label*2, 4])
+
+filename1 = os.path.join(niftidir, initial_surface)
+img1 = nib.load(filename1)
+data1 = img1.get_fdata()
+
+slice_index = np.argwhere(data1 == 1)
+x = slice_index[0, 0]
+y = slice_index[0, 1]
+z = slice_index[0, 2]
+distance_from_aorta = np.power(())
 
 a=1
 # kernel = ball(3)
@@ -213,11 +275,7 @@ a=1
 #     dilated_centerline = np.where(mask > 0, i+1, dilated_centerline)
 
 
-final = centerline
 
-centerline_name = 'centreline_' + niftiroot + '.nii.gz'
-mask = nib.Nifti1Image(final, img1.affine, img1.header)
-nib.save(mask, os.path.join(niftidir, centerline_name))
 
 # kernel = np.array([[[1, 1, 1], [1, 1, 1], [1, 1, 1]],
 #                    [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
